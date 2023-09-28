8db6468654b0ba1bd83e6a1e64c66c3f124d5d67
Johannes Zelenak <jf.zelenak@gmail.com>
2023-09-28 17:53:55 +0200

    add changes

--- script02.js
+++ script02.js
@@ -1,1 +1,20 @@
-document.addEventListener()
+document.getElementById("btn01").addEventListener("click",function() {
+    let message01 = calcTemp1();
+    document.getElementById("output01").innerHTML = message01;
+});
+
+function calcTemp1() {
+    let todaysTemp = randomNumber(-5,25);
+    message01 = "";
+    if (todaysTemp <= 10) {
+        message01 = ` ${todaysTemp}c. The weather is cold`;
+    } else {
+        message01 = ` ${todaysTemp}c. The weather is moderate`;
+    }
+    return message01;
+}
+
+function randomNumber(min, max) {
+    let randomNr = Math.floor(Math.random()*(max - min+1)+min);
+    return randomNr;
+}
